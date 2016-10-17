6128accf8b4deb5c08b35f429f90d2f579c9bd0a
Nishchay Suri <surinishchay@gmail.com>
2016-10-18 00:51:46 +0530

    Commit New

--- openSpin_chain.py
+++ openSpin_chain.py
@@ -17,7 +17,9 @@ import math
 # Set up a Class to compute Hamiltonian for a N atom spin chain with 2^N basis vectors.
 
 class SetUpEquation:
-    def __init__(self,N,k,n_bar,omega,Jx,Jy):
+    def __init__(self,N,k,n_bar,omega,Jx,Jy,t_i,t_f,num_t):
+
+    # Defining the Hamiltonian and the Problem.
         self.N = N
         self.k = k
         self.n_bar = n_bar
@@ -25,6 +27,62 @@ class SetUpEquation:
         self.Jx = Jx
         self.Jy = Jy
         self.num_basis = pow(2,N)
+
+    # Defining Time.
+        self.t_i = t_i
+        self.t_f = t_f
+        self.num_t = num_t
+        
+        self.t = np.linspace(self.t_i, self.t_f, self.num_t)
+        self.dt = self.t[1] - self.t[0]
+        
+        #self.delta = delta
+        #self.eta = eta
+        #self.alpha = alpha
+
+     # Current Time Index.
+        self.t_index = 0
+
+     # Define Standard Basis Matrix
+        self.std_basis = self.Standard()
+        
+        self.Transform_Matrix = np.zeros((self.num_basis**2,self.num_basis**2),dtype='complex')
+
+
+     # Columnized Density Matrices.
+        self.Psi = np.zeros((self.num_t, self.num_basis**2), dtype='complex')
+        self.Psi_Init()
+        self.Chi = np.zeros((self.num_t, self.num_basis**2), dtype='complex')
+
+     # Defining Ex(t) and Ez(t).
+        self.Ex = np.zeros((self.num_t,))
+        self.Ez = np.zeros((self.num_t,))
+
+     # Time Independent Part    
+        self._H0 = self.H0()
+
+
+        '''
+    INITIALIZATION
+
+    '''
+    # Initializes the density column matrix
+    def Psi_Init(self):
+        self.Psi[0,:] = 1
+        return(0)
+
+
+     # Standard Basis Matrix
+    def Standard(self):
+        matrix = np.zeros((self.num_basis**2,self.num_basis**2),dtype='complex')
+        array = np.zeros((self.num_basis**2,))
+        array[0] = 1
+
+        for c in range(0,self.num_basis**2):
+            matrix[:,c] = array
+            array = np.roll(array,1)
+            
+        return(matrix)
 
     # Takes input as an array and generates 2^N dim. sigma matrix by taking the tensor product.
 
@@ -78,6 +136,20 @@ class SetUpEquation:
                     
             return(M)
 
+    # Input String as + or - to produce Sigma+ = sigmaX + i sigmaY and Sigma = sigmaX - i sigmaY
+    def sigma_PlusMinus(self,string):
+        array = np.ones((self.N,))
+        
+        sigmaX = self.Generate_Matrix(array,'sigmaX')
+        sigmaY = self.Generate_Matrix(array,'sigmaY')
+
+        if string == '+':
+            sigma = sigmaX + 1.j * sigmaY
+        elif string == '-':
+            sigma = sigmaX - 1.j * sigmaY
+
+        return(sigma)
+
 
     # H1 = Summation 0.5*omega*(sigmaZ (i)).
     def H1(self):
@@ -103,7 +175,6 @@ class SetUpEquation:
         _Hy = np.zeros((self.num_basis,self.num_basis),dtype='complex')
         
         for i in range(0,self.N-1):
-            print array
             _Hx +=self.Generate_Matrix(array,'sigmaX') 
             _Hy +=self.Generate_Matrix(array,'sigmaY')
             array = np.roll(array,1)
@@ -111,6 +182,13 @@ class SetUpEquation:
         _H2 = (self.Jx * _Hx) + (self.Jy * _Hy)
         
         return(_H2)
+
+
+    # H = H_timeInd + H_timeDep (H is split into a time independent and a time dependant part)
+    
+    def H_timeInd(self):
+        return(self.H1() + self.H2())
+    
 
     # Implementing muX and muY of H(t) = muX Ex(t) + muY Ey(t) 
     def muX(self):
@@ -124,16 +202,16 @@ class SetUpEquation:
 
         return(_muX)
 
-    def muY(self):
+    def muZ(self):
         array = np.zeros((self.N,))
         array[0] = 1
-        _muY = np.zeros((self.num_basis,self.num_basis),dtype='complex')
+        _muZ = np.zeros((self.num_basis,self.num_basis),dtype='complex')
 
         for i in range(0,self.N):
-            _muY += self.Generate_Matrix(array,'sigmaY')
+            _muZ += self.Generate_Matrix(array,'sigmaZ')
             array = np.roll(array,1)
 
-        return(_muY)
+        return(_muZ)
 
 
 
@@ -199,5 +277,92 @@ class SetUpEquation:
             array2 = np.roll(array2,-1)
 
         return(L)
+
+    def Lindblad(self,M1): # Lindblad operator (rho) = M1 rho M1* - 0.5(M1* M1 rho + rho M1* M1)
+
+        M2 = np.conjugate(np.transpose(M1))
+        _Lindblad = self.Op1_rho_Op2(M1,M2) - 0.5 * (self.Op_action_rho(M2*M1) + self.rho_action_Op(M2*M1))
+
+        return(_Lindblad)
+
+    def Lindblad_Thermal(self): # Lindblad_Thermal = L(sqrt(2 k (1+n) sigma-)) + L(sqrt(2 k n sigma+))
+        sigmaMinus = self.sigma_PlusMinus('-')
+        sigmaPlus = self.sigma_PlusMinus('+')
+            
+        _L_thermal = self.Lindblad(np.sqrt(2*self.k*(1+self.n_bar)) * sigmaMinus) +  self.Lindblad(np.sqrt(2*self.k*self.n_bar) * sigmaPlus)
+
+        return(_L_thermal)
+
+
+    '''
+    For Implementing Krotov Algorithm, we separate our rho equation in time dependant and time- independent parts.
+    H0 = Time independent Part which has [rho,H] + Lindblad  (H does not include epsilons)
+    '''
+
+    def H0(self):
+        return(1.j*self.commutator_2_Matrix(self.H_timeInd()) + self.Lindblad_Thermal())
+
+    def Ht(self):
+        t = self.t_index
+        
+        Matrix = self.muX() * self.Ex[t] + self.muZ() * self.Ez[t]
+
+        return(1.j*self.commutator_2_Matrix(Matrix))
     
+    def H(self):
+        return(self._H0 + self.Ht())
+        
+    
+    def Update_Ex_Ez(self):
+        return(0)
+
+
+
+
+    def Update_Transform_Matrix(self,eigVec):
+
+        for r in range(self.num_basis**2):
+            for c in range(self.num_basis**2):
+                self.Transform_Matrix[r][c] = np.vdot(self.std_basis[:,r],eigVec[:,c])
+
+        return(0)
+
+        
+
+    def Update_State(self):
+        t = self.t_index
+        
+        # Diagonalize the Matrix.
+        self.eigVal,self.eigVec = np.linalg.eig(self.H())
+
+        # Update the Transformation Matrix.
+        self.Update_Transform_Matrix(self.eigVec)
+
+        # Calculate the coefficients of Psi in EigenBasis.
+        ket = np.linalg.inv(self.Transform_Matrix).dot(self.Psi[t])
+
+        # Calculate |Psi(t+dt)> by taking the action of the operator
+        ket_new = np.exp(self.eigVal * self.dt) * ket
+
+        # Convert Psi[t+1] back to the standard basis.
+        self.Psi[t+1] = self.Transform_Matrix.dot(ket_new)
+        
+        return(0)
+
+    def Evolution(self):
+        for t in range(0,self.num_t-1):
+            self.t_index = t
+            self.Update_State()
+
+
+    
+
+if __name__ == '__main__':
+    s = SetUpEquation(2,0,0,0,0,0,0,100,100)
+    s.Evolution()
+
+
+
+
+
         
